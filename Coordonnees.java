c52c081dd99950562a477ebe2336728eb5660b33
Matt-TP7 <shiftcode@gmail.com>
2018-01-05 12:00:34 +0100

    La classe du listing de base

---
+++ Coordonnees.java
@@ -0,0 +1,13 @@
+public class Coordonnees{
+  // Les attributs protégés :
+  protected long x;
+  protected long y;
+  // Les setters :
+  public void setX(long x)
+  public void setY(long y)
+  // Les getters :
+  public long getX(long x)
+  public long getY(long y)
+  // Le constructeur par défaut :
+  public Coordonnees()
+}
